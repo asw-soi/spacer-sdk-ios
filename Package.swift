d8c88d7afd354053969fca0c01d2d8b6e97b5c31
t.soi <t.soi@a-sw.jp>
2021-08-04 11:51:59 +0900

    update: SDK

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SpacerSDK",
+    name: "SOILockerSDK",
     platforms: [
         .iOS(.v10),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SpacerSDK",
-            targets: ["SpacerSDK"]),
+            name: "SOILockerSDK",
+            targets: ["SOILockerSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,9 +23,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SpacerSDK",
+            name: "SOILockerSDK",
             dependencies: ["Alamofire"]),
         .testTarget(
-            name: "SpacerSDKTests",
-            dependencies: ["SpacerSDK"]),
+            name: "SOILockerSDKTests",
+            dependencies: ["SOILockerSDK"]),
     ])
---
+++ SOILockerSDK.podspec
@@ -0,0 +1,14 @@
+
+Pod::Spec.new do |spec|
+  spec.name         = "SOILockerSDK"
+  spec.version      = "0.1.0"
+  spec.summary      = "Provides operation using spacers."
+  spec.homepage     = "https://spacer.co.jp"
+  spec.license      = "MIT"
+  spec.author       = "takehito soui"
+  spec.platform     = :ios, "10.2"
+  spec.source       = { :git => "https://github.com/asw-soi/spacer-sdk-ios.git", :tag => "#{spec.version}" }
+  spec.dependency 'Alamofire', '~> 5.4.1'
+  spec.source_files  = "Sources/**/*.{swift}"
+  spec.swift_version = "5.0"
+end
